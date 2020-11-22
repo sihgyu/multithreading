c25dfd4ee88b013a3372a929548fab65b82a04bd
yusx <yusx@jiedaibao.com>
2020-11-22 17:15:03 +0800

    add(day01):创建多线程的方式一： 1、继承Thread类 2、重写run()方法 3、调用start()方法

---
+++ demo/day01/CreateThread.java
@@ -0,0 +1,15 @@
+package day01;
+
+/**
+ * @author yusx
+ * @date 2020/11/22 3:36 下午
+ */
+// 创建多线程的方法 1、继承Thread类 2、重写run()方法 3、调用start()方法
+public class CreateThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("createdThread:" + i);
+        }
+    }
+}
---
+++ demo/day01/Main.java
@@ -0,0 +1,16 @@
+package day01;
+
+/**
+ * @author yusx
+ * @date 2020/11/22 3:45 下午
+ */
+public class Main {
+    public static void main(String[] args) {
+        CreateThread t1 = new CreateThread();
+        t1.start();
+        for (int i = 0; i < 20; i++) {
+            System.out.println("main thread:" + i);
+        }
+        // 线程不一定立即执行，由cpu调度执行
+    }
+}
